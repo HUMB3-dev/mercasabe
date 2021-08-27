dfbea13c07b549acf15303afa3b8e52a92232de0
Randy Tellez Galán <kronhyx@gmail.com>
2021-08-27 00:25:42 -0300

    refactor: add style in main file

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,10 @@
-import '!style-loader!css-loader!sass-loader!./styles.scss'
 import SDK from '@merkaly/sdk-js'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import Vue, { Component } from 'vue'
+import { Component } from 'vue'
 import { getModule } from 'vuex-module-decorators'
+import '../src/main'
 
 import store from '../store'
 import Inventory from '../store/inventory'
-
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 
 SDK.setBaseUrl('https://api.merkaly.io/')
 
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import '@merkaly/ui/src/falcon/index.scss'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from 'vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
