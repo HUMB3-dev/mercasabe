55bace484fff3a020dc9724e7f938d06bcd571c6
HUMB3-dev <humb3rt1co@gmail.com>
2021-09-26 18:14:34 -0400

    LISTO

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    
+    extend: {
+      colors: {
+        redp: '#da4a54',
+        bluep: '#4aafbc',
+        yellowp: '#f28c00',
+        yellows: '#ffd33c',
+        grayp: '#e9e8e8',
+        grays: '#f6f7f9'
+      },
+      width:{
+        xrxr:'48%',
+      },
+      height:{
+        h334:'334px',
+        promoh:'90%',
+      },
+      fontFamily: {
+        'roboto':['Roboto', 'sans-serif'],
+        'robotoc':['Roboto Condensed', 'sans-serif'],
+        'montserrat':['Montserrat', 'sans-serif']
+      
+      },
+      border:{
+        
+      },
+      
+    },
+  
   },
   variants: {
     extend: {},
